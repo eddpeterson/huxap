c6ea8248a37a52eb99a65525389f61587366362f
Edijs Petersons <eddpeterson@gmail.com>
2011-06-29 22:09:08 +0200

    Initial rails app created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Huxap::Application.config.secret_token = '01e568d9494077d6b05cb38e6075750a70e101c2fc1f5b11b9966a28faa0c00e80ff4cc0fcc97acc8886ff3835159cbef3a919288b380b2ac0539e9d71870d3d'
